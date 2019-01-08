9bd9dd16a24c4f55ae583d9ca7a1e0da3913dc4a
Kevin Morelli <kev9318@gmail.com>
2019-01-08 10:41:24 -0300

    Renamed folders

---
+++ 2018/03 - PowerShell Workshop/SystemLogLast15.ps1
@@ -0,0 +1,7 @@
+﻿<#
+ultimos 15 eventos del log "System"
+#>
+
+
+$logs = Get-EventLog -Newest 15 -Log System
+$logs | Format-List -Property *
--- PowerShell Workshop 2018/SystemLogLast15.ps1
+++
@@ -1,7 +0,0 @@
-﻿<#
-ultimos 15 eventos del log "System"
-#>
-
-
-$logs = Get-EventLog -Newest 15 -Log System
-$logs | Format-List -Property *
