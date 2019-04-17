af97370f910b692554b62911b5584e3067406d85
Kevin Morelli <kev9318@gmail.com>
2019-04-16 21:23:08 -0300

    Reorganize files

--- 2019/08 - Flutter Introduction/hello_flutter/lib/main.dart
+++ 2019/08 - Flutter Introduction/hello_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,62 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hello Flutter App',
       home: MyHomePage(title: 'Hidden Message App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int counter = 0;
-  var template = 'Hello Flutter!';
-  var message = '';
-
-  void incrementCounter() {
-    setState(() {
-      if (counter < template.length) {
-        counter++;
-        message = '';
-
-        for (int i = 0; i < counter; i++) {
-          message += template[i];
-        }
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Material(
-        color: Colors.deepPurple,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('$message',
-                  style: TextStyle(color: Colors.white, fontSize: 24.0)),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
