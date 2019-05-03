d58facae5b12cf2d5c0e6636bd88902dac6250dd
Kevin Morelli <kev9318@gmail.com>
2019-05-02 21:55:01 -0300

    Bloc pattern

---
+++ 2019/08 - Flutter Introduction/bloc_architecture/lib/counter_bloc.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:bloc_architecture/counter_provider.dart';
+
+class CounterBloc {
+  final counterController = StreamController(); // create a StreamController
+  final CounterProvider provider =
+      CounterProvider(); // create an instance of our CounterProvider
+
+  Stream get getCount =>
+      counterController.stream; // create a getter for our stream
+
+  void updateCount() {
+    provider
+        .increaseCount(); // call the method to increase our count in the provider
+    counterController.sink.add(provider.count); // add the count to our sink
+  }
+
+  void dispose() {
+    counterController.close(); // close our StreamController
+  }
+}
+
+final bloc = CounterBloc(); // create an instance of the counter bloc
